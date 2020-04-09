002a9eff7fd428c1eb315c6279c54e065115500d
Pramod KC <49179488+Pramodbucky@users.noreply.github.com>
2020-04-09 17:38:34 +0545

    delete config file

--- configlist.py
+++
@@ -1,4 +0,0 @@
-CHROME_WEBDRIVER = "C://Users//pkc//Desktop//Pramod//Sky_Geek_Selenium_UnitTest//chromedriver_win32//chromedriver.exe"
-SKY_GEEK_LOGIN_USERNAME = "Skygeekadmin"
-SKY_GEEK_LOGIN_PASSWORD = "Skygeek1"
-
